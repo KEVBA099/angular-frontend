3fcb83ca8965df1d49adcd357fd2d4b511bee18c
KEVBA099 <kramirezb3@miumg.edu.gt>
2024-10-07 17:56:42 -0600

    Initial commit of angular frontend

---
+++ angular-frontend/src/app/services/greeting.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GreetingService {
+    private apiUrl = 'http://localhost:8080/api/greet';  // URL de la API
+
+    constructor(private http: HttpClient) { }
+
+    getGreeting(): Observable<string> {
+        return this.http.get(this.apiUrl, { responseType: 'text' });
+    }
+}
